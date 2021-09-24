600fbad8f91487b6c5b3e75b8474e7ecbe53c240
radha <radhavkrishna1994@gmail.com>
2021-09-24 14:34:46 +0530

    updated jms

--- JmsNewExample/src/main/java/com/training/MyController.java
+++ JmsNewExample/src/main/java/com/training/MyController.java
@@ -2,6 +2,7 @@ package com.training;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +15,17 @@ public class MyController {
 	@Autowired
 	private JmsTemplate jmsTemplate;
 	
-	@PostMapping("/mq/publishitem")
+	@PostMapping("/mq/email")
 	public Email publishItem(@RequestBody Email email)
 	{
 		jmsTemplate.convertAndSend("testQueue",email);
 		return email;
 		
+	}
+	
+	@GetMapping("/hello")
+	public String hello()
+	{
+		return "hello";
 	}
 }
