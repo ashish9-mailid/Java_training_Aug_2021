a990d3489cbf66815d6a8191fab65d9333f98dcf
radha <radhavkrishna1994@gmail.com>
2021-09-27 18:39:28 +0530

    Spring boot upload example added

---
+++ SpringBootUploadExample/src/main/java/com/training/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,28 @@
+package com.training.uploadingfiles;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.training.uploadingfiles.storage.StorageProperties;
+import com.training.uploadingfiles.storage.StorageService;
+
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
